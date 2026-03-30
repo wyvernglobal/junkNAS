0357bd82711e36a8ef2eb17b712a9af1d2a36c88
josh <josh@Sasukette.powerhub>
2026-03-30 13:23:42 +0100

    Added per disk allocation

--- build/CMakeFiles/junknas_daemon.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/junknas_daemon"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/junknas_daemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
